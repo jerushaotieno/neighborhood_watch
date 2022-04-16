f29b6f8c154d19e46e4187e53f5b1d82b05a7d63
jerushaotieno <jerushaotienocoding@gmail.com>
2022-04-16 17:14:57 +0300

    create neighborhood model class and accompanying methds

--- hoodwatch/models.py
+++ hoodwatch/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+# Model for Neighborhood Class
+
+class Neighborhood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=60)
+    admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
+    image = models.ImageField(upload_to='images/')
+    description = models.TextField()
+    local_hospital = models.IntegerField(null=True, blank=True)
+    police_number = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def create_neighborhood(self):
+        self.save()
+
+    def delete_neighborhood(self):
+        self.delete()
+
+    @classmethod
+    def find_neighborhood(cls, neighborhood_id):
+        return cls.objects.filter(id=neighborhood_id)
